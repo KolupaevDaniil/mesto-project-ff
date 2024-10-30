8884a906ebc48772c4bd31804e67368fc1448eeb
Daniil Kolupaev <kolupayevd@inbox.ru>
2024-10-30 12:18:29 +0300

    fix:- card.js index.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,36 +1,23 @@
 //для функций для работы с карточками(создание, лайк, удаление)
-import { openModal } from "./modal";
+import { openPopupCard } from "../scripts/index";
 export {createCard, openPopupCard, likeCard, deleteCard};
 const cardTemplate = document.querySelector('#card-template').content;
 
 function createCard(cardItem, deleteCard, openPopupCard, likeCard) {
     const card = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const likeButton = card.querySelector('.card__like-button');
+    const deleteButton = card.querySelector('.card__delete-button');
     
     cardImage.src = cardItem.link;
     cardImage.alt = cardItem.name;
-    card.querySelector('.card__title').textContent = cardItem.name;
+    cardTitle.textContent = cardItem.name;
+    likeButton.addEventListener('click', likeCard);
+    deleteButton.addEventListener('click', () => deleteCard(card));
+    cardImage.addEventListener('click', () => openPopupCard(cardItem.link, cardItem.name));
 
-    card.addEventListener('click', (evt) => {
-        if (evt.target.className === 'card__image')
-            openPopupCard(cardItem.link, cardItem.name);
-
-        if (evt.target.classList.contains('card__like-button'))
-            likeCard(evt);
-
-        if (evt.target.className === 'card__delete-button')
-            deleteCard(card);
-    });
     return card;
-}
-
-
-function openPopupCard(link, name) {
-    const popupImage = document.querySelector('.popup_type_image');
-    popupImage.querySelector('.popup__image').src = link;
-    popupImage.querySelector('.popup__image').alt = name;
-    popupImage.querySelector('.popup__caption').textContent = name;
-    openModal(popupImage);
 }
 
 function likeCard(evt) {
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,43 @@
 import '../pages/index.css';
 import {initialCards} from './cards';
-import {createCard, openPopupCard, likeCard, deleteCard} from '../components/card';
+import {createCard, likeCard, deleteCard} from '../components/card';
 import {openModal} from '../components/modal';
 
 const content = document.querySelector('.content');
 const placesList = document.querySelector('.places__list');
 const popupProfile = document.querySelector('.popup_type_edit');
 const popupNewCard = document.querySelector('.popup_type_new-card');
+const popupImage = document.querySelector('.popup_type_image');
 const inputName = popupProfile.querySelector('.popup__input_type_name');
 const inputDescription = popupProfile.querySelector('.popup__input_type_description');
 const profileTitle = content.querySelector('.profile__title');
 const profileDescription = content.querySelector('.profile__description');
 
+const profileEditButton = document.querySelector('.profile__edit-button');
+const newCardAddButton = document.querySelector('.profile__add-button');
+
 initialCards.forEach(cardItem => {
     placesList.append(createCard(cardItem, deleteCard, openPopupCard, likeCard));
 }); 
 
-function handlePageSectionButton(evt) {
-    if (evt.target.className === "profile__edit-button") {
-        inputName.value = profileTitle.textContent;
-        inputDescription.value = profileDescription.textContent;
-        openModal(popupProfile);
-    }
-    if (evt.target.className === "profile__add-button")
-        openModal(popupNewCard);
+export function openPopupCard(link, name) {
+    popupImage.querySelector('.popup__image').src = link;
+    popupImage.querySelector('.popup__image').alt = name;
+    popupImage.querySelector('.popup__caption').textContent = name;
+    openModal(popupImage);
 }
-document.addEventListener('click', handlePageSectionButton);
+
+function openProfilePopup() {
+    inputName.value = profileTitle.textContent;
+    inputDescription.value = profileDescription.textContent;
+    openModal(popupProfile);
+}
+profileEditButton.addEventListener('click', openProfilePopup);
+
+function openNewCardPopup() {
+    openModal(popupNewCard);
+}
+newCardAddButton.addEventListener('click', openNewCardPopup);
 
 function handleProfileSubmit(evt) {
     evt.preventDefault();
